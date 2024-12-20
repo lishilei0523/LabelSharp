47b47dc0ba409d0a37dd85f66adde5ccbe9ca489
Lee <lishilei0523@163.com>
2024-12-21 06:25:47 +0800

    -

--- src/LabelSharp/ViewModels/HomeContext/IndexViewModel.Draw.cs
+++ src/LabelSharp/ViewModels/HomeContext/IndexViewModel.Draw.cs
@@ -14,7 +14,6 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using Point = System.Windows.Point;
 
 namespace LabelSharp.ViewModels.HomeContext
 {

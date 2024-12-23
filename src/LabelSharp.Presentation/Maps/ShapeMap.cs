7b135273af46444478d3d32a010613b6f32d33d1
Lee <lishilei0523@163.com>
2024-12-23 15:37:11 +0800

    临时提交；

--- src/LabelSharp.Presentation/Maps/ShapeMap.cs
+++ src/LabelSharp.Presentation/Maps/ShapeMap.cs
@@ -1,5 +1,8 @@
-﻿using SD.Infrastructure.Shapes;
+﻿using LabelSharp.Presentation.Models;
+using SD.Infrastructure.Shapes;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LabelSharp.Presentation.Maps
 {
@@ -189,5 +192,141 @@ namespace LabelSharp.Presentation.Maps
             return mePoints;
         }
         #endregion
+
+        #region # 映射点 —— static PointL ToPointL(this IList<double[]> mePoints)
+        /// <summary>
+        /// 映射点
+        /// </summary>
+        public static PointL ToPointL(this IList<double[]> mePoints)
+        {
+            int x = (int)Math.Ceiling(mePoints[0][0]);
+            int y = (int)Math.Ceiling(mePoints[0][1]);
+            PointL pointL = new PointL(x, y);
+
+            return pointL;
+        }
+        #endregion
+
+        #region # 映射线 —— static LineL ToLineL(this IList<double[]> mePoints)
+        /// <summary>
+        /// 映射线
+        /// </summary>
+        public static LineL ToLineL(this IList<double[]> mePoints)
+        {
+            int x1 = (int)Math.Ceiling(mePoints[0][0]);
+            int y1 = (int)Math.Ceiling(mePoints[0][1]);
+            int x2 = (int)Math.Ceiling(mePoints[1][0]);
+            int y2 = (int)Math.Ceiling(mePoints[1][1]);
+            PointL pointA = new PointL(x1, y1);
+            PointL pointB = new PointL(x2, y2);
+            LineL lineL = new LineL(pointA, pointB);
+
+            return lineL;
+        }
+        #endregion
+
+        #region # 映射矩形 —— static RectangleL ToRectangleL(this IList<double[]> mePoints)
+        /// <summary>
+        /// 映射矩形
+        /// </summary>
+        public static RectangleL ToRectangleL(this IList<double[]> mePoints)
+        {
+            int x1 = (int)Math.Ceiling(mePoints[0][0]);
+            int y1 = (int)Math.Ceiling(mePoints[0][1]);
+            int x3 = (int)Math.Ceiling(mePoints[2][0]);
+            int y3 = (int)Math.Ceiling(mePoints[2][1]);
+            RectangleL rectangleL = new RectangleL(x1, y1, x3 - x1, y3 - y1);
+
+            return rectangleL;
+        }
+        #endregion
+
+        #region # 映射圆形 —— static CircleL ToCircleL(this IList<double[]> mePoints)
+        /// <summary>
+        /// 映射圆形
+        /// </summary>
+        public static CircleL ToCircleL(this IList<double[]> mePoints)
+        {
+            int x1 = (int)Math.Ceiling(mePoints[0][0]);
+            int y1 = (int)Math.Ceiling(mePoints[0][1]);
+            int x2 = (int)Math.Ceiling(mePoints[1][0]);
+            CircleL circleL = new CircleL(x1, y1, x2 - x1);
+
+            return circleL;
+        }
+        #endregion
+
+        #region # 映射椭圆形 —— static EllipseL ToEllipseL(this IList<double[]> mePoints)
+        /// <summary>
+        /// 映射椭圆形
+        /// </summary>
+        public static EllipseL ToEllipseL(this IList<double[]> mePoints)
+        {
+            int x1 = (int)Math.Ceiling(mePoints[0][0]);
+            int y1 = (int)Math.Ceiling(mePoints[0][1]);
+            int x2 = (int)Math.Ceiling(mePoints[1][0]);
+            int y3 = (int)Math.Ceiling(mePoints[2][1]);
+            EllipseL ellipseL = new EllipseL(x1, y1, x2 - x1, y3 - y1);
+
+            return ellipseL;
+        }
+        #endregion
+
+        #region # 映射多边形 —— static PolygonL ToPolygonL(this IList<double[]> mePoints)
+        /// <summary>
+        /// 映射多边形
+        /// </summary>
+        public static PolygonL ToPolygonL(this IList<double[]> mePoints)
+        {
+            IEnumerable<PointL> pointLs =
+                from mePoint in mePoints
+                let x = (int)Math.Ceiling(mePoint[0])
+                let y = (int)Math.Ceiling(mePoint[1])
+                select new PointL(x, y);
+            PolygonL polygonL = new PolygonL(pointLs);
+
+            return polygonL;
+        }
+        #endregion
+
+        #region # 映射多边形 —— static PolylineL ToPolylineL(this IList<double[]> mePoints)
+        /// <summary>
+        /// 映射多边形
+        /// </summary>
+        public static PolylineL ToPolylineL(this IList<double[]> mePoints)
+        {
+            IEnumerable<PointL> pointLs =
+                from mePoint in mePoints
+                let x = (int)Math.Ceiling(mePoint[0])
+                let y = (int)Math.Ceiling(mePoint[1])
+                select new PointL(x, y);
+            PolylineL polylineL = new PolylineL(pointLs);
+
+            return polylineL;
+        }
+        #endregion
+
+
+        public static Annotation ToAnnotation(this MeShape meShape)
+        {
+            //Shape shape;
+            //if (meShape.ShapeType==Constants.MePoint)
+            //{
+            //    PointL pointL = meShape.Points.ToPointL();
+            //    shape = new PointVisual2D
+            //    {
+            //        X = pointL.X,
+            //        Y = pointL.Y,
+            //        Fill = new SolidColorBrush(Colors.Black),
+            //        Stroke = new SolidColorBrush(this.BorderColor!.Value),
+            //        RenderTransform = canvas.MatrixTransform,
+            //        Tag = pointL
+            //    };
+            //}
+
+            Annotation annotation = new Annotation(meShape.Label, meShape.GroupId, meShape.Truncated, meShape.Difficult, null, meShape.Description);
+
+            return annotation;
+        }
     }
 }
